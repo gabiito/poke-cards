847afbadb9755736db3b3d62586ec769463be788
Gabriel Zerbino <gabozerbino@gmail.com>
2021-07-27 21:46:48 -0300

    detalles y funciones

--- js/main.js
+++ js/main.js
@@ -1,43 +1,42 @@
 const colors = ["#B97A95", "#F6AE99", "#B5CDA3", "#F38BA0", "#C1AC95", "#5F939A", "#94D0CC", "#F1CA89", "#CC9B6D", "#FAF0AF", "#F69E7B"];
 
-let pokemonAPI = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=";
-
-const cantPorPagina = "12";
+let pokemonAPI = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=14";
 
 const container = document.getElementById("app");
 
 
 
-async function getPokemons (url, cantPokemon){
+async function getPokemonList (url){
     try{
-        const resp = await fetch(url + cantPokemon);
+        const resp = await fetch(url);
         const data = await resp.json();
-        const pokemons = {
-            prev: data.previous,
-            next: data.next,
-            pokeArr: data.results
-        }
-        return pokemons;
+        return data;
     }
     catch(e){
         return e;
     }
-    
 }
 
-let pokemons = getPokemons(pokemonAPI, cantPorPagina);
-console.log(pokemons)
+async function getPokemonData (url){
+    try{
+        const resp = await fetch(url);
+        const data = await resp.json();
+        return data
+    }
+    catch (e){
+        return e;
+    }
+}
 
-function fillContent(pokeArray){
-
-    for (pokemon of pokeArray){
+function fillContent(pokemon){
         container.innerHTML += `
         <div class="card">
             <div class="card__header">
                 <div class="card__header--top"></div>
+                <div class="card__header--num">${pokemon.id}</div>
                 <img src="${pokemon.sprites.front_default}" class="card__header--img">
                 <h2>${pokemon.name}</h2>
-                <small>WEIGHT: ${pokemon.weigth}Kg</small>
+                <small>WEIGHT: ${pokemon.weight}Kg</small>
             </div>
             
             <div class="card__body">
@@ -47,7 +46,7 @@ function fillContent(pokeArray){
                     ${pokemon.stats[0].base_stat}
                 </div>
                 <div class="card__body__stat stat--attack" style="width:${pokemon.stats[1].base_stat >= 100 ?  100 : pokemon.stats[1].base_stat}%;">
-                    <i class="fas fa-bomb"></i>
+                    <i class="fas fa-crosshairs"></i>
                     ${pokemon.stats[1].base_stat}
                 </div>
                 <div class="card__body__stat stat--defense" style="width:${pokemon.stats[2].base_stat >= 100 ?  100 : pokemon.stats[2].base_stat}%;">
@@ -60,11 +59,59 @@ function fillContent(pokeArray){
                 </div>
             </div>
         </div>
-        `
-    }
+        `;
+        
     const tops = document.querySelectorAll(".card__header--top");
     for (let top of tops){
         let rand = Math.floor(Math.random() * colors.length);
         top.style.background = colors[rand];
     }
 }
+
+var showSpinner = function(){
+    document.getElementById("spinner-wrapper").style.display = "block";
+  }
+  
+  var hideSpinner = function(){
+    document.getElementById("spinner-wrapper").style.display = "none";
+  }
+
+let loader = function (url){
+    try {
+        getPokemonList(url).then(res =>{
+            pokemonAPI = res;
+
+            res.results.forEach(pokemon =>{
+                getPokemonData(pokemon.url).then(data => fillContent(data));
+            });
+            hideSpinner();
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+document.addEventListener("DOMContentLoaded", (evt) =>{
+    
+    showSpinner();
+    loader(pokemonAPI)
+});
+
+
+document.getElementById("next").addEventListener("click", (evt) =>{
+    evt.preventDefault();
+    container.innerHTML = "";
+    
+    showSpinner();
+    loader(pokemonAPI.next);
+});
+
+document.getElementById("prev").addEventListener("click", (evt) =>{
+    evt.preventDefault();
+    
+    
+    if(pokemonAPI && pokemonAPI.previous != null){
+        container.innerHTML = "";
+        showSpinner();
+        loader(pokemonAPI.previous);
+    }
+});
